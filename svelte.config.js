48eaaf47dbd75c7a055c01373ed66eb439cca7bf
irg1008 <ivansudevlop@gmail.com>
2022-06-14 13:31:26 +0200

    Changed adaptar for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +11,9 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			edge: true
+		})
 	}
 };
 
