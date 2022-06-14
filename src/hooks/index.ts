a5d116bc5b46a2a442d3a1ae50fb36ed85b651d9
irg1008 <ivansudevlop@gmail.com>
2022-06-14 19:21:36 +0200

    Changed admin route protection

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,23 +1,34 @@
 import userService from '$lib/db/services/user.service';
 import { getCookie, getUserCookie } from '$lib/utils/cookie';
 import type { GetSession, Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = async ({ event, resolve }) => {
+const getUser: Handle = async ({ event, resolve }) => {
 	const userId = getCookie(event.request, 'user');
 	if (userId) event.locals.user ??= await userService.get(userId);
-
-	const { user } = event.locals;
-	const isAdminRoute = event.url.pathname.startsWith('/admin');
-	if (isAdminRoute && user?.role !== 'ADMIN')
-		return new Response(`${user?.role}`, {
-			status: 402,
-			headers: { location: '/' }
-		});
-
 	const response = await resolve(event);
 	response.headers.set('Set-Cookie', getUserCookie(event.locals.user?.id ?? ''));
 	return response;
 };
+
+const routeProtection: Handle = async ({ event, resolve }) => {
+	const {
+		locals: { user },
+		url
+	} = event;
+
+	const isAdminRouteAndNotAdmin = url.pathname.startsWith('/admin') && user?.role !== 'ADMIN';
+
+	if (isAdminRouteAndNotAdmin)
+		return new Response(`Unauthorized`, {
+			status: 302,
+			headers: { location: '/' }
+		});
+
+	return await resolve(event);
+};
+
+export const handle = sequence(getUser, routeProtection);
 
 export const getSession: GetSession = async ({ locals: { user } }) => {
 	return { user };
