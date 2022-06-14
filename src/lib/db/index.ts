d3c3991183691e59adf8f78f0b475135af456484
irg1008 <ivansudevlop@gmail.com>
2022-06-14 13:57:27 +0200

    Added global for prisma object

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-const db = new PrismaClient();
+const db = global.db || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') global.db = db;
 
 export { db };
