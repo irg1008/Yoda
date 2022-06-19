bb213c27adf2de91fe67dee01e78a938b84180b0
irg1008 <ivansudevlop@gmail.com>
2022-06-19 14:14:56 +0200

    Moved admin route redirect to layout page. Not the best bur for the momento should do. This works because load function is url-reactive, and handle hook is not

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,24 +1,10 @@
 import { userService } from '$lib/db/services';
 import { getCookie, getUserCookie } from '$lib/utils/cookie';
 import type { GetSession, Handle } from '@sveltejs/kit';
-import { sequence } from '@sveltejs/kit/hooks';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const userId = getCookie(event.request, 'user');
 	if (userId) event.locals.user ??= await userService.get(userId);
-
-	const {
-		locals: { user },
-		url
-	} = event;
-
-	const isAdminRoute = url.pathname.startsWith('/admin');
-	const isAdmin = user?.role === 'ADMIN';
-	if (isAdminRoute && !isAdmin)
-		return new Response(null, {
-			status: 302,
-			headers: { location: `/${isAdmin}` }
-		});
 
 	const response = await resolve(event);
 	response.headers.set('Set-Cookie', getUserCookie(event.locals.user?.id ?? ''));
@@ -28,30 +14,3 @@ export const handle: Handle = async ({ event, resolve }) => {
 export const getSession: GetSession = async ({ locals: { user } }) => {
 	return { user };
 };
-
-// const getUser: Handle = async ({ event, resolve }) => {
-// 	const userId = getCookie(event.request, 'user');
-// 	if (userId) event.locals.user ??= await userService.get(userId);
-// 	const response = await resolve(event);
-// 	response.headers.set('Set-Cookie', getUserCookie(event.locals.user?.id ?? ''));
-// 	return response;
-// };
-
-// const routeProtection: Handle = async ({ event, resolve }) => {
-// 	const {
-// 		locals: { user },
-// 		url
-// 	} = event;
-
-// 	const isAdminRouteAndNotAdmin = url.pathname.startsWith('/admin') && user?.role !== 'ADMIN';
-
-// 	if (isAdminRouteAndNotAdmin)
-// 		return new Response(`Unauthorized`, {
-// 			status: 302,
-// 			headers: { location: '/' }
-// 		});
-
-// 	return await resolve(event);
-// };
-
-// export const handle = sequence(getUser, routeProtection);
---
+++ src/routes/admin/__layout.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = ({ session }) => {
+		console.log('reaching here');
+		const isAdmin = session.user?.role === 'ADMIN';
+		if (!isAdmin) {
+			return {
+				status: 302,
+				redirect: '/'
+			};
+		}
+
+		return {};
+	};
+</script>
+
+<slot />
